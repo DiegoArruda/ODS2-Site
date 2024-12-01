e997607c42e37dc7ad0dc503ce310a019c41f0ae
DiegoArruda <arrudadiego1@gmail.com>
2024-12-01 15:57:55 -0300

    Edição da página de Contatos

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -5,37 +5,36 @@ const contacts = [
 			"O projeto Vizinhos Solidários é um instituto dedicado a fornecer refeições e cestas básicas na região do grande Recife e Olinda.",
 		contact:
 			"Link: <a href='https://vizinhossolidarios.ong.br/' target='_blank'>https://vizinhossolidarios.ong.br/</a>",
+		image: "../src/logo.png",
 	},
 
-	{
-		name: "Recife Solidário",
-		description:
-			"Eureciclo trabalha com logística reversa de embalagens, ajudando empresas a compensar o impacto ambiental das embalagens de seus produtos por meio da reciclagem.",
-		contact: "Email: comercial@eureciclo.com.br",
-	},
 	{
 		name: "Armazém do Campo do Recife",
 		description:
 			"o Armazém do Campo do Recife, mantido pelo Movimento dos Trabalhadores Sem Terra (MST) iniciou a produção de marmitas solidárias na cozinha do estabelecimento para alimentar pessoas que vivem em situação de rua.",
 		contact: "Instagram: @armazemdocamporecife",
+		image: "../src/logo_cabecalho.png",
 	},
 	{
 		name: "Central Única das Favelas",
 		description:
 			"A Central Única das Favelas (Cufa) promove ações de mobilização e apoio aos moradores de favelas de todo o Brasil. A organização atua em mais de 5 mil favelas do paístrabalho de doaos e donativos com a chegada da pandemia.",
 		contact: "Instagram: @cufape",
+		image: "../src/cufa300.webp",
 	},
 	{
 		name: "Sintraci",
 		description:
 			"O Sindicato dos Trabalhadores e Trabalhadoras do Comércio Informal do Recife (Sintraci) está promovendo a doação de cestas básicas para camelôs e ambulantes da cidade.",
 		contact: "Instagram: @sintraci.sindicato",
+		image: "../src/sintraci.jpg",
 	},
 	{
 		name: "Coração Quentinho Recife ",
 		description:
 			"O Coração Quentinho Recife, nasceu durante a pandemia, onde ajudam com alimentos prontos, as pessoas em situação de vulnerabilidade como também, assistem várias famílias com Cestas Básicas em 5 comunidade de Recife",
 		contact: "Instagram: @ongcoracaoquentinhorecife",
+		image: "../src/coracao_quentinho.jpeg",
 	},
 ];
 
@@ -46,6 +45,7 @@ function loadContacts() {
 		contactCard.className = "contact-card";
 		contactCard.innerHTML = `
             <h2>${contact.name}</h2>
+			<img src="${contact.image}" alt="Imagem de ${contact.name}" class="contact-image" />
             <p>${contact.description}</p>
             <p class="contact-info">${contact.contact}</p>
         `;
